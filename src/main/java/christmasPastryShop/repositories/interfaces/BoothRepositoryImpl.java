4b193c9ab12514c682a986fde89efa4e9840779d
Vladimir Zorov <vl.zorov@gmail.com>
2023-01-06 18:36:00 +0200

    Add changes

---
+++ src/main/java/christmasPastryShop/repositories/interfaces/BoothRepositoryImpl.java
@@ -0,0 +1,33 @@
+package christmasPastryShop.repositories.interfaces;
+
+import christmasPastryShop.entities.booths.interfaces.Booth;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
+public class BoothRepositoryImpl implements BoothRepository<Booth>{
+
+    private Collection<Booth> booths;
+
+    public BoothRepositoryImpl() {
+        this.booths = new ArrayList<>();
+    }
+
+    @Override
+    public Booth getByNumber(int number) {
+        return booths.stream().filter(b -> b.getBoothNumber()==number)
+                .findFirst()
+                .orElse( null);
+    }
+
+    @Override
+    public Collection<Booth> getAll() {
+        return Collections.unmodifiableCollection(this.booths);
+    }
+
+    @Override
+    public void add(Booth booth) {
+        booths.add(booth);
+    }
+}
